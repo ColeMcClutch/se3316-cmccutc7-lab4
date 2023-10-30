8a377caf171c66cf532589b295c6966479cb2117
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2023-10-30 17:39:40 -0400

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,40 +1,3 @@
-const express = require('express');
-const fs = require('fs');
-const app = express()
-const port = 3000;
-
-
-//Setting up front-end code
-app.use('/', express.static('static'));
-
-//Superhero_info app
-app.get('/api/superhero_info', (req,res)=> {
-    fs.readFile('superhero_info.json', (err, data) => {
-        if (err) {
-          res.status(500).send('Error reading the JSON file');
-        } else {
-          const infoData = JSON.parse(data);
-          res.json(jsonData);
-        }
-})
-})
-
-//Superhero_powers app
-app.get('/api/superhero_powers', (req,res)=> {
-    fs.readFile('superhero_powers.json', (err, data) => {
-        if (err) {
-          res.status(500).send('Error reading the JSON file');
-        } else {
-          const infoData = JSON.parse(data);
-          res.json(powersData);
-        }
-})
-})
-
-app.listen(port, ()=>{
-    console.log('listening on port ${port} ')
-})
-
 
 //Button creations
 const search = document.getElementById('searchBar')
@@ -48,8 +11,6 @@ const view = document.getElementById('viewer')
 
 
 //Button commands
-
-
 //search
 if (searchFilter=="name"){}
 else if (searchFilter="race"){}
