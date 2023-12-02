2418141f23630c3782e8e7c254f4e48d2a7df1ac
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2023-12-02 00:19:06 -0500

    Update listMenu.js

--- client/my-react-app/src/ListMenu/listMenu.js
+++ client/my-react-app/src/ListMenu/listMenu.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './listMenu.css';
+import { useNavigate } from 'react-router-dom';
+
 
 const ListMenu = () => {
   // State variables
@@ -7,6 +9,18 @@ const ListMenu = () => {
   const [listName, setListName] = useState('');
   const [listType, setListType] = useState('public');
   const [selectedList, setSelectedList] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState(''); 
+  const [newPassword, setNewPassword] = useState(''); // Add password state
+  const navigate = useNavigate()
+  const [ratingChoice, setRatingChoice] = useState('');
+  const [ratingText, setRatingText] = useState('');
+  const [review, setReview] = useState('');
+  const [listDescription, setListDescription] = useState(''); 
+
+
+
 
   // Event handlers (replace with your logic)
   const handleSearch = () => {
@@ -29,11 +43,120 @@ const ListMenu = () => {
     // Implement delete hero from list logic
   };
 
+  //Update password
+  const updatePassword = async () => {
+    try {
+      const updateResponse = await fetch('/api/users/updatePassword', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email, 
+          password: password, 
+          nickname: username, 
+          newPassword: newPassword
+        }),
+      });
+
+      if (updateResponse.ok) {
+        const success = await updateResponse.json()
+        console.log(success)
+      } else {
+        // Handle failure
+        console.log('Unable to change Password, check credientials')
+      }
+    } catch (error) {
+      console.error('Error during password update:', error);
+    }
+  };
+
+  const rateList = () => {
+    //Rating List method
+  }
+
+  const logOut = () => {
+    navigate('/login')
+  }
+
   return (
     <div>
       <div className="title">
         <h1>Cole's List of Heroes</h1>
       </div>
+
+    <div className = "uPOptions">
+      Use the textboxes for updating passwords
+      <input type = "text"
+            id = 'emailUse'
+            placeholder = 'Inspect email'
+            value = {email}
+            onChange={(e) => setEmail(e.target.value)} // Update password state
+            />
+
+      <input type = "text"
+            id = 'passwordUse'
+            placeholder = 'Password'
+            value = {password}
+            onChange={(e) => setPassword(e.target.value)} // Update password state
+            />
+
+      <input type = "text"
+            id = 'usernameUse'
+            placeholder = 'Username'
+            value = {username}
+            onChange={(e) => setUsername(e.target.value)} // Update password state
+            />
+
+      <input type = "text"
+            id = 'newpasswordUse'
+            placeholder = 'New Password'
+            value = {newPassword}
+            onChange={(e) => setNewPassword(e.target.value)} // Update password state
+            />
+
+      <button type="button" id="updatePassword" onClick={updatePassword}>
+            Update Password
+            </button>
+        </div>
+
+        <div className='="ratingOptions'>
+
+        <select
+        id="ratingChoice"
+        value={ratingChoice}
+        onChange={(e) => setListType(e.target.value)}
+        >
+        </select>
+
+
+          <input type = "text"
+          id = 'listRate'
+          placeholder = 'Rate out of 5'
+          value = {ratingText}
+          onChange={(e) => setRatingChoice(e.target.value)}
+          />
+
+          <input type = "text"
+          id = 'listReview'
+          placeholder = 'Comments?'
+          value = {review}
+         onChange={(e) => setReview(e.target.value)}
+          />
+
+        <button type="button" id="rateButton" onClick={rateList}>
+         Rate List
+        </button>
+
+
+        <button type="button" id="logoutButton" onClick={logOut}>
+        LogOut
+        </button>
+
+
+        </div>
+
+
 
       <div className="searchMethods">
   <input
@@ -99,14 +222,22 @@ const ListMenu = () => {
           value={listName}
           onChange={(e) => setListName(e.target.value)}
         />
+        <input
+          type="text"
+          id="newDescription"
+          name="newDescription"
+          placeholder="List Description: "
+          value={listDescription}
+          onChange={(e) => setListName(e.target.value)}
+        />
         <label htmlFor="listChoice">Public or Private List?:</label>
         <select
           id="listChoice"
           value={listType}
           onChange={(e) => setListType(e.target.value)}
         >
-          <option value="public">Public</option>
           <option value="private">Private</option>
+          <option value="public">Public</option>
         </select>
 
         <label htmlFor="deleteDropdown">Select list:</label>
