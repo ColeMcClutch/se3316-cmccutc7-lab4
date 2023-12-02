3a7c03ecbf111d6bdfd543cb0bb0548d095ab3e1
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2023-12-02 00:18:51 -0500

    Create AdminView.js

---
+++ client/my-react-app/src/AdminView/AdminView.js
@@ -0,0 +1,14 @@
+// AuthenticatedView.js
+import React from 'react';
+import AdminMenu from '../adminMainPage/adminMainPage.js';
+
+const AdminView = () => {
+  return (
+    <div>
+      <h1>Welcome to the authenticated view!</h1>
+      <AdminMenu />
+    </div>
+  );
+};
+
+export default AdminView;
