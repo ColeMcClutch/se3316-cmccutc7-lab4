02a48a27cd0747e68ac0d039cb199161a41c0903
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2023-12-02 00:18:57 -0500

    Create AuthenticatedView.js

---
+++ client/my-react-app/src/AuthenticatedView/AuthenticatedView.js
@@ -0,0 +1,14 @@
+// AuthenticatedView.js
+import React from 'react';
+import ListMenu from '../ListMenu/listMenu.js';
+
+const AuthenticatedView = () => {
+  return (
+    <div>
+      <h1>Welcome to the authenticated view!</h1>
+      <ListMenu />
+    </div>
+  );
+};
+
+export default AuthenticatedView;
