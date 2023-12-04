3e688b177fdb8fc3aa21018fa35d9331f35b0a0f
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2023-12-04 17:36:19 -0500

    Update AuthenticatedView.js

--- client/my-react-app/src/AuthenticatedView/AuthenticatedView.js
+++ client/my-react-app/src/AuthenticatedView/AuthenticatedView.js
@@ -3,10 +3,15 @@ import React from 'react';
 import ListMenu from '../ListMenu/listMenu.js';
 
 const AuthenticatedView = ({username}) => {
+  const { username: usernameValue } = username;
+
+  console.log('Original username:', username);
+
+
   return (
     <div>
       <h1>Welcome to the authenticated view!</h1>
-      <ListMenu username={{username}}/>
+      <ListMenu username={username}/>
     </div>
   );
 };
