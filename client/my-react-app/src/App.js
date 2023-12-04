d45e124e7c1453970faac44668b138c12408bf1c
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2023-12-04 17:36:13 -0500

    Update App.js

--- client/my-react-app/src/App.js
+++ client/my-react-app/src/App.js
@@ -1,5 +1,5 @@
 // App.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './login/login.js';
 import AuthenticatedView from './AuthenticatedView/AuthenticatedView.js';
@@ -9,19 +9,27 @@ import { AuthProvider } from './AuthContext'; // Adjust the import path
 const App = () => {
   const [loggedInUsername, setLoggedInUsername] = useState('');
 
+  useEffect(() => {
+    // Check if there's a username in localStorage
+    const storedUsername = localStorage.getItem('loggedInUsername');
+    if (storedUsername) {
+      setLoggedInUsername(storedUsername);
+    }
+  }, []);
+
   const handleLogin = (username) => {
     setLoggedInUsername(username);
   };
 
   return (
+    <AuthProvider>
       <Routes>
-        <Route path="/login" element={<Login onLogin={handleLogin} />} />
-        <Route
-          path="/authenticated"
-          element={<AuthProvider><AuthenticatedView username={loggedInUsername} /></AuthProvider>}
-        />
+      <Route path="/login" element={<Login onLogin={handleLogin} />} />
+        <Route path="/authenticated" element={<AuthenticatedView username={loggedInUsername} />} />
         <Route path="/admin" element={<AdminView />} />
+        <Route path="/" element={<Login onLogin={handleLogin} />} />
       </Routes>
+    </AuthProvider>
   );
 };
 
