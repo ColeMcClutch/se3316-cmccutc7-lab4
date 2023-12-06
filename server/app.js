7faff48141f7e7e171500fe85d8b66f6b1f7f934
cmccutc7 <114160057+ColeMcClutch@users.noreply.github.com>
2023-12-06 11:42:22 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const users = new nodeStorage("users/users.json")
 const express = require('express');
 const app = express();
 
-const port = 80;
+const port = 3000;
 const superheroInfo = require('./superheroes/superhero_info.json')
 const superheroPowers =require('./superheroes/superhero_powers.json')
 
